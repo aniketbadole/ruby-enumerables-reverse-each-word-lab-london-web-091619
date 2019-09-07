25cf6dbc70798ee12191035f659d4b1abac09a6a
Aniket B <aniket.b@outlook.com>
2019-09-07 18:40:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,12 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  
-  reversed = array.each do { |i| i.reverse! } 
+  reversed = array.each { |i| i.reverse! } 
   return reversed.join (" ")
+end
+
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  reversed_array = array.each {|x| x.reverse!}
+  return reversed_array.join(" ")
 end
